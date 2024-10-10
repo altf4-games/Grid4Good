de93959da932858efb6459d5f1867eebcacf03d9
Pradyum Mistry <39883432+altf4-games@users.noreply.github.com>
2024-10-10 17:49:52 +0530

    Contract

---
+++ contract/contracts/Contract.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Grid {
+    event PixelUpdated(uint256 x, uint256 y, address owner, uint256 color);
+
+    struct Pixel {
+        address owner;
+        uint256 color;
+    }
+
+    uint256 public constant GRID_SIZE = 1000;
+    uint256 public constant PIXEL_PRICE = 0.01 ether;
+
+    mapping(uint256 => mapping(uint256 => Pixel)) public pixels;
+
+    constructor() {}
+
+    function buyPixel(uint256 x, uint256 y, uint256 color) external payable {
+        require(msg.value >= PIXEL_PRICE, "Insufficient Ether to buy pixel");
+        require(x < GRID_SIZE && y < GRID_SIZE, "Invalid coordinates");
+
+        Pixel storage pixel = pixels[x][y];
+
+        if (pixel.owner == address(0)) {
+            pixel.owner = msg.sender;
+        } else {
+            require(pixel.owner == msg.sender, "You do not own this pixel");
+        }
+
+        pixel.color = color;
+
+        emit PixelUpdated(x, y, msg.sender, color);
+    }
+
+    function getPixel(
+        uint256 x,
+        uint256 y
+    ) external view returns (address, uint256) {
+        require(x < GRID_SIZE && y < GRID_SIZE, "Invalid coordinates");
+        Pixel storage pixel = pixels[x][y];
+        return (pixel.owner, pixel.color);
+    }
+
+    function withdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
